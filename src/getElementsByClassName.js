d15c4c0f5be18d655b54207ece5d2c7313f32096
Vivek Tumrukota <Vivek@viveks-air.attlocal.net>
2015-03-15 18:38:55 -0700

    Finished Stringify + getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var nodeList = [];
+  var nodes = document.body.childNodes; 
+
+  var classMatch = function(currentNode){
+    if (currentNode.classList) {
+      for(var i=0; i<currentNode.classList.length; i++){
+        if(currentNode.classList[i] === className){
+          nodeList.push(currentNode);
+          break;
+        }
+      }  
+    }
+    for(var i=0; i<currentNode.childNodes.length; i++){
+      classMatch(currentNode.childNodes[i]);
+    }
+  }
+
+  classMatch(document.body)
+
+  return nodeList;
 };
+
+getElementsByClassName('example');
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,81 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var string = '';
+
+	// Null
+	if (obj === null) {
+		return 'null';
+	}
+	//Undefined
+	else if(typeof(obj) === 'undefined'){
+		return undefined;
+	}
+	//functions
+	else if(typeof(obj) === 'function'){
+		return undefined;
+	}
+	// String
+	else if(typeof(obj) !== 'object'){
+		if(typeof(obj) ==='string'){
+			return '"' + obj + '"'; 
+		}
+		return obj.toString();
+	}
+
+	// Array
+	else if(obj.constructor === Array){
+		string += '[';
+		var firstitem = true;
+
+		for(var i=0; i<obj.length; i++){
+			if (firstitem) {
+				firstitem = false;
+			}
+			else {
+				string += ',';
+			}
+
+			if(typeof(obj[i]) === 'function' || typeof(obj[i]) ==='undefined'){
+				string += null;
+			}
+
+			string += stringifyJSON(obj[i]);
+
+		}
+
+		string += ']';
+
+	}
+
+	// Object
+	else if (typeof(obj) === 'object') {
+		string += '{';
+		var first = true;
+
+		for (var key in obj) {
+			if (typeof(obj[key]) !== 'undefined' && typeof(obj[key]) !== 'function') {
+				// Handle commas
+				if (first) {
+					first = false;
+				}
+				else {
+					string += ',';
+				}
+
+				// Key
+				string += '"' + key + '":';
+
+				string += stringifyJSON(obj[key]);
+			}
+		}
+
+		string += '}';
+	}
+
+	return string;
+
 };
+
